b8e8fd3667e456d2192c8eb228f5b808f45701ea
Wirayabovorn007 <wirayabovorn606@gmail.com>
2026-04-02 16:19:06 +0700

    Update philo

--- philosopers/philo/main.c
+++ philosopers/philo/main.c
@@ -1,38 +1,127 @@
 
 #include "philo.h"
 
-void	init(t_program *prog, char *argv[])
+size_t	get_curr_time()
 {
-	int		opt_arg_val;
-	t_philo	*philos;
+	struct	timeval	time;
 
-	philos = prog->philos;
-	philos[ft_atoi(argv[0])];
-	opt_arg_val = 0;
-	if (argv[4])
-		opt_arg_val = argv[4];
-	philos->number_of_philo = ft_atoi(argv[0]);
-	philos->time_to_die = ft_atoi(argv[1]);
-	philos->time_to_eat = ft_atoi(argv[2]);;
-	philos->time_to_sleep = ft_atoi(argv[3]);
-	philos->times_philo_eat = opt_arg_val;
+	if (gettimeofday(&time, NULL) == -1)
+		printf("Error getting time\n");
+	return ((time.tv_sec * 1000) + (time.tv_sec / 1000));
 }
 
-void philosopher(char *argv[])
+void	monitor(t_program *prog)
+{
+	int	i;
+	int	all_full;
+
+	while (1)
+	{
+		i = 0;
+		all_full = 1;
+
+		while (i < prog->num_of_philos)
+		{
+			pthread_mutex_lock(&prog->philos[i].meals_eaten);
+			if (get_curr_time() - prog->philos[i].last_meal >= prog->time_to_die)
+			{
+				// pthread_mutex_unlock(&prog->philos[i].meal_lock) --> meal_lock doesn't exist
+			}
+			if (prog->num_times_to_eat != -1 && prog->philos[i].meals_eaten < prog->num_times_to_eat)
+				all_full = 0;
+			i++;
+		}
+		if (prog->num_times_to_eat != -1 && all_full == 1)
+        {
+            return;
+        }
+	}
+}
+
+
+void	init_prog(t_program *prog, char *argv[], int argc)
+{
+	prog->num_of_philos = ft_atoi(argv[0]);
+	prog->time_to_die = ft_atoi(argv[1]);
+	prog->time_to_eat = ft_atoi(argv[2]);
+	prog->time_to_sleep = ft_atoi(argv[3]);
+	if (argc == 6)
+		prog->num_times_to_eat = ft_atoi(argv[4]);
+	else
+		prog->num_times_to_eat = -1;
+	prog->dead_flag = 0;
+	pthread_mutex_init(&prog->dead_lock, NULL);
+	pthread_mutex_init(&prog->write_lock, NULL);
+
+	return (0);
+}
+
+void	init_philos_and_forks(t_program *prog)
+{
+	int	i;
+
+	prog->philos = malloc(sizeof(t_philo) * prog->num_of_philos);
+	prog->forks = malloc(sizeof(pthread_mutex_t) * prog->num_of_philos);
+	if (!prog->philos || !prog->forks)
+		return (1);
+	i = 0;
+	while (i < prog->num_of_philos)
+	{
+		pthread_mutex_init(&prog->forks[i], NULL);
+		i++;
+	}
+	i = 0;
+	while (i < prog->num_of_philos)
+	{
+		prog->philos[i].id = i + 1;
+		prog->philos[i].meals_eaten = 0;
+		prog->philos[i].prog = prog;
+		prog->philos[i].l_fork = &prog->forks[i];
+		prog->philos[i].r_fork = &prog->forks[(i + 1) % prog->num_of_philos];
+	}
+}
+
+void	*philo_routine(void *arg)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *) arg;
+	printf("Philosopher %d has starting their routine!\n", philo->id);
+
+	return (NULL);
+}
+
+int	simulation(t_program *prog)
+{
+	int	i;
+	pthread_t	thread_id;
+
+	i = 0;
+	while (i < prog->num_of_philos)
+	{
+		if (pthread_create(&thread_id, NULL, &philo_routine, &prog->philos[i] != 0))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	philosopher(char *argv[], int argc)
 {
 	t_program	prog;
 
-	init(&prog, argv);
+	init_prog(&prog, argv + 1, argc);
+	init_philos_and_forks(&prog);
 }
 
 
 int main(int argc, char *argv[])
 {
 	// check error
-	if (argc - 1 < 4)
+	if (argc < 5 || argc > 6)
 		return (1);
-	argv++;
-	if (!check_input(argv, argc))
+	if (!check_input(argv + 1, argc))
 		return (1);
-	philosopher(argv);
+	if (philosopher(argv, argc) != 0)
+		return (1);
 }
--- philosopers/philo/philo.h
+++ philosopers/philo/philo.h
@@ -3,24 +3,31 @@
 
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <sys/time.h>
 #include <pthread.h>
 
 
 typedef struct s_philo {
-	int		idx;
-	int number_of_philo;
-	float	time_to_die;
-	float	time_to_eat;
-	float	time_to_sleep;
-	int		times_philo_eat;
+	int				id;                 // Philosopher number (1 to N)
+	int				meals_eaten;        // Track how many times they ate
+	size_t			last_meal;          // Timestamp of the last meal
+	pthread_mutex_t	*l_fork;            // Pointer to left fork mutex
+	pthread_mutex_t	*r_fork;            // Pointer to right fork mutex
+	t_program		*prog;              // Pointer to the shared program data
+}	t_philo;
 
-	pthread_mutex_t	*r_fork;
-	pthread_mutex_t	*l_fork;
-} t_philo;
-
-typedef struct s_program
-{
-	t_philo	*philos;
+typedef struct s_program {
+	int				num_of_philos;
+	size_t			time_to_die;
+	size_t			time_to_eat;
+	size_t			time_to_sleep;
+	int				num_times_to_eat;   // -1 if not specified
+	int				dead_flag;          // 1 if someone died, 0 otherwise
+	pthread_mutex_t	dead_lock;          // Protects dead_flag
+	pthread_mutex_t	write_lock;         // Protects printf outputs
+	pthread_mutex_t	*forks;             // Array of fork mutexes
+	t_philo			*philos;            // Array of philosophers
 }	t_program;
 
 
