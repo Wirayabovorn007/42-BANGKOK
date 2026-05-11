d10bd957456b3610aac5c6b79a72517e5dfa10f1
Wirayabovorn007 <wirayabovorn606@gmail.com>
2026-05-11 17:36:20 +0700

    Update

---
+++ exam/rank03/level1/filter/filter.c
@@ -0,0 +1,65 @@
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int is_heekuytad(char *str, char *format, int curr_i)
+{
+	int j = 0;
+
+	while (format[j])
+	{
+		if (str[curr_i] != format[j]) return 0;
+		j++;
+		curr_i++;
+	}
+	return 1;
+}
+
+void print_star(int len_format)
+{
+	int i =-1;
+
+	while (++i < len_format)
+		printf("*");
+}
+
+void ft_filter(char *str, char *format)
+{
+	int i = 0;
+	int len_format = strlen(format);
+
+	while (str[i])
+	{
+		if (str[i] == format[0])
+		{
+			if (is_heekuytad(str, format, i))
+			{
+				print_star(len_format);
+				i+=len_format;
+				continue ;
+			}
+		}
+		printf("%c", str[i]);
+		i++;
+	}
+}
+
+int main(int ac , char **av)
+{
+	if (ac != 2 || !av[1]) return 1;
+	
+	char *buf = malloc(sizeof(char) * 1024);
+	if (!buf) return 1;
+	ssize_t bytes_read = read(0, buf, 1024);
+	if (bytes_read <= 0)
+	{
+		free(buf);
+		return 0;
+	}
+	ft_filter(buf, av[1]);
+	free(buf);
+	return 0;
+}
---
+++ exam/rank03/level1/scanf/scanf.c
@@ -0,0 +1,148 @@
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <ctype.h>
+
+int match_space(FILE *f)
+{
+	int c;
+
+	while ((c = fgetc(f)) != EOF)
+	{
+		if (!isspace(c))
+		{
+			ungetc(c, f);
+			break;
+		}
+	}
+	if (ferror(f)) return -1;
+    return (0);
+}
+
+int match_char(FILE *f, char c)
+{
+	int r = fgetc(f);
+	if (r == c) return 1;
+	if (r != EOF)
+		ungetc(r, f);
+    return (0);
+}
+
+int scan_char(FILE *f, va_list ap)
+{
+	int c = fgetc(f);
+	if (c == EOF) return -1;
+	char *ptr = va_arg(ap, char *);
+	*ptr = c;
+    return (0);
+}
+
+int scan_int(FILE *f, va_list ap)
+{
+	int c = fgetc(f);
+	int sign = 1;
+	int res = 0;
+
+	if (c == EOF) return -1;
+	if (c == '-' || c ==' +') {
+		if (c == '-') {
+			sign = -1;
+			c = fgetc(f);
+		}
+	}
+	if (c == EOF || !isdigit(c))
+	{
+		if (c != EOF) ungetc(c, f);
+		return 0;
+	}
+
+	while (c != EOF && isdigit(c))
+	{
+		res = res * 10 + (c - '0');
+		c = fgetc(f);
+	}
+	if (c != EOF) ungetc(c, f);
+	int *ptr = va_arg(ap, int *);
+	*ptr = res * sign;
+    return (1);
+}
+
+int scan_string(FILE *f, va_list ap)
+{
+	int c = fgetc(f);
+	if (c == EOF) return -1;
+	int count = 0;
+	char *ptr = va_arg(ap, char *);
+	while (c != EOF && !isspace(c))
+	{
+		ptr[count++] = c;
+		c = fgetc;
+	}
+	if (c != EOF) ungetc(c, f);
+	ptr[count] = '\0';
+    return (0);
+}
+
+
+int	match_conv(FILE *f, const char **format, va_list ap)
+{
+	switch (**format)
+	{
+		case 'c':
+			return scan_char(f, ap);
+		case 'd':
+			match_space(f);
+			return scan_int(f, ap);
+		case 's':
+			match_space(f);
+			return scan_string(f, ap);
+		case EOF:
+			return -1;
+		default:
+			return -1;
+	}
+}
+
+int ft_vfscanf(FILE *f, const char *format, va_list ap)
+{
+	int nconv = 0;
+
+	int c = fgetc(f);
+	if (c == EOF)
+		return EOF;
+	ungetc(c, f);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (match_conv(f, &format, ap) != 1)
+				break;
+			else
+				nconv++;
+		}
+		else if (isspace(*format))
+		{
+			if (match_space(f) == -1)
+				break;
+		}
+		else if (match_char(f, *format) != 1)
+			break;
+		format++;
+	}
+	
+	if (ferror(f))
+		return EOF;
+	return nconv;
+}
+
+
+int ft_scanf(const char *format, ...)
+{
+	va_list ap;
+	va_start(ap, format);
+	int ret = ft_vfscanf(stdin, format, ap);
+	va_end(ap);
+	return ret;
+}
